40b4b4a87d5337c92fe27612556b32d42d934c01
mskumramdhavarapu <mskumarmadhavarapu@gmail.com>
2026-02-24 20:29:14 +0530

    functions

--- for_each/r53.tf
+++ for_each/r53.tf
@@ -8,3 +8,15 @@ resource "aws_route53_record" "www" {
   records = [each.value.private_ip]
   allow_overwrite = true
 }
+
+# roboshop.mskdaws88s.online -> public_ip
+resource "aws_route53_record" "www" {
+  for_each  =   aws_instance.example
+  zone_id = var.zone_id
+  # interpolation
+  name    = "roboshop.${var.domain_name}" # monogodb.mskdaws88s.onlie
+  type    = "A"
+  ttl     = 1
+  records = [lookup(aws_instance.example, "frontend").public_ip]
+  allow_overwrite = true
+}
