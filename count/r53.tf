27c1a55625149a37e11137cd262e110a8b1ccbed
mskumramdhavarapu <mskumarmadhavarapu@gmail.com>
2026-02-24 20:28:56 +0530

    functions

--- count/r53.tf
+++ count/r53.tf
@@ -7,3 +7,15 @@ resource "aws_route53_record" "www" {
   ttl     = 1
   records = [aws_instance.example[count.index].private_ip]
 }
+
+# roboshop.mskdaws88s.online -> public_ip
+
+resource "aws_route53_record" "www" {
+  count   = 10
+  zone_id = var.zone_id
+  # interpolation
+  name    = "roboshop.${var.domain_name}" 
+  type    = "A"
+  ttl     = 1
+  records = [aws_instance.example[index(var.instances, "frontend")].private_ip]
+}
